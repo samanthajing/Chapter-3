75d662ca8737ed781586195a214a4b00282813ea
samanthajing <samanthajing6529@gmail.com>
2026-09-17 12:25:56 -0700

    Add files via upload

--- Convert.java
+++ Convert.java
@@ -5,10 +5,11 @@ public class Convert {
 	public static void main(String[] args) {
 		
 		final double FACTOR = 9.0/5.0; 
+		final double CONSTANT = 32.0;
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter a temperature in Celsius: ");
 		int celsius = in.nextInt();
-		double fahrenheit = celsius*FACTOR+32.0;
+		double fahrenheit = celsius*FACTOR + CONSTANT;
 		System.out.print(celsius + " C = " + fahrenheit + " F");
 	}
 
